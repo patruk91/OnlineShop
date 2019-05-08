ae5949d61e6fe4a5682b75b296ff69732b43c8b4
Krzysiek <krzysiekczaja@gmail.com>
2019-05-08 14:23:17 +0200

    Add methods in View interface and implement them in TextView class

--- src/main/java/com/codecool/viewer/textViewer/TextView.java
+++ src/main/java/com/codecool/viewer/textViewer/TextView.java
@@ -3,4 +3,28 @@ package com.codecool.viewer.textViewer;
 import com.codecool.viewer.View;
 
 public class TextView implements View {
+    @Override
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public void displayQuestion(String question) {
+        System.out.println(question + "?");
+    }
+
+    @Override
+    public void displayError(String error) {
+        System.out.println("Error: " + error);
+    }
+
+    @Override
+    public void displayTable(String table) {
+        System.out.println(table);
+    }
+
+    @Override
+    public void displeyMenu(String menu) {
+        System.out.println(menu);
+    }
 }
