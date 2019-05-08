7f6a935715d6e9a18924a36a7fe8506d98e9ac83
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-08 13:13:34 +0200

    Implement product class: constructor and toString method

--- src/main/java/com/codecool/model/Product.java
+++ src/main/java/com/codecool/model/Product.java
@@ -1,4 +1,23 @@
 package com.codecool.model;
 
 public class Product {
+    private int productId;
+    private String name;
+    private double price;
+    private int amount;
+
+    public Product(int productId, String name, double price, int amount) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "productId: " + productId
+                + ", name: " + name
+                + ", price: " + price
+                + ", amount: " + amount;
+    }
 }
