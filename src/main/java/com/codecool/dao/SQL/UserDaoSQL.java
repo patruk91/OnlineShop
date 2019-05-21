dbffd6c0788029b6e1e0b58585cc985073971d94
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-05-21 10:16:52 +0200

    Implement interface UserDao, add methods into UserDaoSQL class

--- src/main/java/com/codecool/dao/SQL/UserDaoSQL.java
+++ src/main/java/com/codecool/dao/SQL/UserDaoSQL.java
@@ -1,6 +1,29 @@
 package com.codecool.dao.SQL;
 
 import com.codecool.dao.UserDao;
+import com.codecool.model.User;
+
+import java.util.List;
 
 public class UserDaoSQL implements UserDao {
+
+    @Override
+    public void createUser(User user) {
+        
+    }
+
+    @Override
+    public List<User> readUser(int userId) {
+        return null;
+    }
+
+    @Override
+    public void updateUser(User user, String column) {
+
+    }
+
+    @Override
+    public void deleteUser(User user) {
+
+    }
 }
--- src/main/java/com/codecool/dao/UserDao.java
+++ src/main/java/com/codecool/dao/UserDao.java
@@ -1,4 +1,12 @@
 package com.codecool.dao;
 
+import com.codecool.model.User;
+
+import java.util.List;
+
 public interface UserDao {
+    void createUser(User user);
+    List<User> readUser(int userId);
+    void updateUser(User user, String column);
+    void deleteUser(User user);
 }
